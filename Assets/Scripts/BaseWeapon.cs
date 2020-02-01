81bd22160d3e136b0c08330a27350ef5618c3a39
Imran Bepari <imranbepari97@gmail.com>
2020-02-01 12:17:20 +0000

    started weapons

---
+++ Assets/Scripts/BaseWeapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseWeapon : MonoBehaviour
+{
+    bool isFixed;
+    public enum WeaponDefect {
+        Blunt, 
+        Unpolished,
+        Broken,
+        Bent,
+        ArrowThrough
+    }
+
+    public WeaponDefect defect;  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
