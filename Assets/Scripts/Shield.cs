bff8ec5151fd71a0d7adc3a8ed20e5a3eda9b07f
ImranBepari97 <imranbepari97@gmail.com>
2020-02-01 22:36:39 +0000

    shielf

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : BaseWeapon
+{
+
+    public GameObject arrow1;
+    public GameObject arrow2;
+    public GameObject arrow3;
+
+    int arrowExitCount; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        
+        if(containsDefect(WeaponDefect.ArrowThrough)) {
+            arrow1.gameObject.SetActive(true);
+            arrow2.gameObject.SetActive(true);
+            arrow3.gameObject.SetActive(true);
+            arrowExitCount = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+
+    }
+
+    void OnTriggerExit(Collider col) {
+        if(col.gameObject.Equals(arrow1) || col.gameObject.Equals(arrow2) || col.gameObject.Equals(arrow2) ) {
+            col.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            arrowExitCount += 1;
+            Debug.Log("Arrow removed");
+            if(arrowExitCount > 2) {
+                defects.Remove(WeaponDefect.ArrowThrough);
+            }
+        } 
+
+
+    }
+
+}
