330713cc042f6ceebcb241df4b756f2b397f5d70
Sam <sam.cooley19@gmail.com>
2020-02-01 23:31:59 +0000

    Revert "Added sharpen effect"
    This reverts commit 869547b63a4167a04d589780056ee1f11942367a.

--- Assets/SwordSharpening.cs
+++ Assets/SwordSharpening.cs
@@ -4,18 +4,10 @@ using UnityEngine;
 
 public class SwordSharpening : MonoBehaviour
 {
-    public float TotalTime = 6f;
-    public float TimeSharpened = 0f;
-    private Renderer rend;
-
-
     // Start is called before the first frame update
     void Start()
     {
-        rend = GetComponent<Renderer>();
-
-        // Use the Specular shader on the material
-        //rend.material.shader = Shader.Find("custom/Blend");
+        
     }
 
     // Update is called once per frame
@@ -23,23 +15,4 @@ public class SwordSharpening : MonoBehaviour
     {
         
     }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (collision.collider.tag != "Grindstone") return;
-        if (TimeSharpened >= TotalTime) return;
-
-        TimeSharpened += Time.deltaTime;
-
-        //float amount = Mathf.Lerp(0f, TotalTime, TimeSharpened);
-
-
-        //rend.material.SetFloat("_Blend", amount);
-
-        TimeSharpened += Time.deltaTime; // add time each frame
-        float percentComplete = TimeSharpened / TotalTime; // value between 0 - 1
-        percentComplete = Mathf.Clamp01(percentComplete); // this prevents it exceeding 1
-        rend.material.SetFloat("_Blend", percentComplete);
-    }
 }
-
