4c075fbe1a9691a090c4b1bf8174bd38fb007d35
Sule <solibepari@hotmail.com>
2020-02-02 11:55:04 +0000

    Auto stash before merge of "sound" and "origin/master"

---
+++ Assets/Scripts/SoundScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+
+AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!source.isPlaying) {
+            source.pitch=Random.Range(0.8f,1.2f);
+            source.Play();
+            
+        }
+        
+    }
+}
