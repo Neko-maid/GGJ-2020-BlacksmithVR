40d8830b7f76d6e81dcdf977d70e26136936ea15
Sam <sam.cooley19@gmail.com>
2020-02-02 12:06:04 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] WeaponPrefabs;
+    public Transform SpawnLocation;
+
+    private bool IsWeaponActive = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void SpawnPrefab(GameObject weapon)
+    {
+        Instantiate(weapon, SpawnLocation.transform.position, Quaternion.identity);
+    }
+}
